839a0dfe422c93613f7f0a9371c26fc98c0ec651
alyalqarni <30639922+alyalqarni@users.noreply.github.com>
2018-02-02 10:07:38 -0500

    Add files via upload

--- 1_Introduction/Alya/src.example/ofApp.cpp
+++ 1_Introduction/Alya/src.example/ofApp.cpp
@@ -2,7 +2,7 @@
 
 //--------------------------------------------------------------
 void ofApp::setup(){
-
+    ofBackground(255, 255, 255);
 }
 
 //--------------------------------------------------------------
@@ -12,8 +12,64 @@ void ofApp::update(){
 
 //--------------------------------------------------------------
 void ofApp::draw(){
-  ofSetColor(255,0,0);
-  ofDrawCircle(300, 300, 100);
+    ofSetColor(20, 23, 124);
+    ofDrawCircle(100, 100, 60);
+    
+    ofSetColor(123, 0, 200);
+    ofDrawBezier(10, 50, 342, 324, 234, 312, 231, 323);
+    
+    ofSetColor(34, 345, 23);
+    ofDrawRectangle(10, 10, 100, 100);
+    
+    ofSetColor(20, 123, 200);
+    ofDrawTriangle(200, 200, 200, 123, 12, 21);
+    
+    ofSetColor(200, 23, 124);
+    ofDrawCircle(300, 240, 78);
+    
+    
+    ofSetColor(255, 255, 255);
+    ofDrawCurve(120, 100, 90, 90, 700, 720, 740, 100);
+    
+    ofSetColor(234, 23, 124);
+    ofDrawLine(7, 40, 260, 40);
+    
+
+    ofSetColor(209, 100, 123);
+    ofDrawBitmapString("where is the rest of the shapes?", 50, 60);
+    
+    ofSetColor(209, 100, 123);
+    ofDrawBitmapString("where is the rest of the shapes?", 50, 80);
+    
+    ofSetColor(209, 100, 123);
+    ofDrawBitmapString("where is the rest of the shapes?", 50, 100);
+    
+    ofSetColor(209, 100, 123);
+    ofDrawBitmapString("where is the rest of the shapes?", 50, 110);
+    
+    ofSetColor(209, 100, 123);
+    ofDrawBitmapString("where is the rest of the shapes?", 50, 115);
+    
+    ofSetColor(209, 100, 123);
+    ofDrawBitmapString("where is the rest of the shapes?", 50, 135);
+    
+    ofSetColor(209, 100, 123);
+    ofDrawBitmapString("where is the rest of the shapes?", 50, 155);
+    
+    ofSetColor(209, 100, 123);
+    ofDrawBitmapString("where is the rest of the shapes?", 50, 175);
+    
+    ofSetColor(209, 100, 123);
+    ofDrawBitmapString("where is the rest of the shapes?", 50, 195);
+    
+    ofSetColor(209, 100, 123);
+    ofDrawBitmapString("where is the rest of the shapes?", 50, 215);
+    
+    ofSetColor(209, 100, 123);
+    ofDrawBitmapString("where is the rest of the shapes?", 50, 225);
+    
+    ofSetColor(209, 100, 123);
+    ofDrawBitmapString("where is the rest of the shapes?", 50, 245);
 }
 
 //--------------------------------------------------------------
@@ -27,7 +83,7 @@ void ofApp::keyReleased(int key){
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseMoved(int x, int y){
+void ofApp::mouseMoved(int x, int y ){
 
 }
 
--- 1_Introduction/Alya/src.example/ofApp.h
+++ 1_Introduction/Alya/src.example/ofApp.h
@@ -3,14 +3,15 @@
 #include "ofMain.h"
 
 class ofApp : public ofBaseApp{
+
 	public:
 		void setup();
 		void update();
 		void draw();
-		
+
 		void keyPressed(int key);
 		void keyReleased(int key);
-		void mouseMoved(int x, int y);
+		void mouseMoved(int x, int y );
 		void mouseDragged(int x, int y, int button);
 		void mousePressed(int x, int y, int button);
 		void mouseReleased(int x, int y, int button);
@@ -19,4 +20,5 @@ class ofApp : public ofBaseApp{
 		void windowResized(int w, int h);
 		void dragEvent(ofDragInfo dragInfo);
 		void gotMessage(ofMessage msg);
+		
 };
