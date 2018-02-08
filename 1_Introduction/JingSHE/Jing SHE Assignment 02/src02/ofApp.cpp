e9ed4073f9c92b8f8f2c7cf17d8d6d035893bda0
JingSHE <jings474@newschool.edu>
2018-02-08 12:17:13 -0500

    This is the second assignment

---
+++ 1_Introduction/JingSHE/Jing SHE Assignment 02/src02/ofApp.cpp
@@ -0,0 +1,332 @@
+#include "ofApp.h"
+
+//--------------------------------------------------------------
+void ofApp::setup(){
+    
+  
+}
+
+//--------------------------------------------------------------
+void ofApp::update(){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::draw(){
+     ofBackground(255, 255, 255);
+   
+    ofSetColor(230, 143, 197 );
+
+    ofDrawTriangle(220, 70, 320, 70, 344, 95);
+    
+    ofDrawTriangle(186, 70, 290, 90, 225, 86);
+    
+    ofDrawTriangle(186, 90, 341, 100, 290, 105);
+    
+    ofDrawTriangle(184, 98, 270, 108, 127, 117);
+    
+    ofDrawTriangle(96, 130, 278, 110, 237, 134);
+    
+    ofDrawTriangle(124, 140, 55, 203, 160, 138);
+    
+    ofDrawTriangle(35, 233, 137, 162, 91, 205);
+    
+    ofDrawTriangle(64, 246, 170, 142, 216, 142);
+    
+    ofDrawTriangle(88, 240, 230, 142, 200, 175);
+    
+    ofDrawTriangle(30, 316, 106, 235, 77, 290);
+    
+    ofDrawTriangle(64, 346, 115, 232, 184, 191);
+    
+    ofDrawTriangle(55, 370, 198, 186, 176, 250);
+    
+    ofDrawTriangle(62, 377, 167, 268, 145, 322);
+    
+    ofDrawTriangle(96, 366, 145, 331, 102, 420);
+    
+    ofDrawTriangle(103, 454, 175, 262, 174, 362);
+    
+    ofDrawTriangle(134, 425, 170, 380, 149, 480);
+    
+    ofSetColor(143, 189, 190);
+    
+    ofDrawTriangle(168, 456, 185, 340, 202, 497);
+    
+    ofDrawTriangle(190, 340, 200, 448, 205, 383);
+    
+    ofDrawTriangle(210, 405, 268, 510, 210, 475);
+    
+    ofDrawTriangle(211, 390, 256, 466, 326, 490);
+    
+    ofDrawTriangle(263, 483, 367, 509, 290, 507);
+    
+    ofDrawTriangle(290, 451, 413, 480, 362, 495);
+    
+     ofDrawTriangle(373, 467, 500, 429, 426, 475);
+    
+     ofDrawTriangle(390, 495, 470, 470, 437, 495);
+    
+     ofDrawTriangle(470, 431, 512, 377, 499, 420);
+    
+    ofDrawTriangle(488, 392, 493, 336, 505, 367);
+    
+    ofSetColor(205, 197, 177);
+    
+    ofDrawTriangle(375, 109, 352, 141, 391, 135);
+    
+    ofDrawTriangle(296, 145, 365, 120, 348, 143);
+
+    ofDrawTriangle(241, 140, 350, 124, 206, 175);
+    
+    ofDrawTriangle(206, 180, 291, 148, 180, 254);
+    
+    ofDrawTriangle(181, 258, 287, 155, 182, 325);
+    
+    ofDrawTriangle(233, 250, 294, 148, 359, 144);
+    
+    ofDrawTriangle(229, 254, 305, 193, 185, 327);
+    
+    ofDrawTriangle(185, 333, 305, 197, 274, 279);
+    
+    ofDrawTriangle(188, 335, 273, 282, 211, 381);
+    
+    ofDrawTriangle(213, 383, 267, 297, 249, 416);
+    
+    ofDrawTriangle(251, 416, 265, 325, 287, 440);
+    
+    ofDrawTriangle(273, 343, 292, 443, 349, 454);
+    
+    ofDrawTriangle(296, 369, 354, 454, 403, 450);
+    
+    ofDrawTriangle(352, 406, 408, 448, 445, 433);
+    
+    ofDrawTriangle(415, 421, 448, 431, 472, 413);
+    
+    ofDrawTriangle(308, 189, 362, 144, 358, 170);
+    
+    ofDrawTriangle(309, 191, 319, 208, 358, 174);
+    
+    ofDrawTriangle(321, 210, 269, 167, 345, 230);
+    
+    ofDrawTriangle(349, 231, 373, 164, 365, 233);
+    
+    ofDrawTriangle(367, 232, 375, 162, 397, 204);
+    
+    ofDrawTriangle(402, 206, 419, 254, 420, 202);
+    
+    ofDrawTriangle(421, 254, 422, 206, 436, 253);
+    
+    ofDrawTriangle(423, 255, 436, 255, 448, 274);
+    
+    ofDrawTriangle(421, 254, 422, 206, 436, 253);
+    
+    ofDrawTriangle(402, 204, 408, 161, 421, 200);
+    
+    ofDrawTriangle(402, 204, 408, 161, 421, 200);
+    
+    ofDrawTriangle(288, 111, 354, 121, 245, 135);
+    
+    ofDrawTriangle(288, 111, 354, 121, 352, 104);
+    
+    ofDrawTriangle(365, 144, 377, 158, 361, 171);
+    
+    ofDrawTriangle(365, 144, 377, 158, 380, 150);
+    
+    ofDrawTriangle(365, 144, 390, 144, 380, 150);
+    
+    ofDrawTriangle(369, 167, 345, 230, 321, 210);
+    
+    ofDrawTriangle(367, 235, 399, 205, 386, 245);
+    
+    ofDrawTriangle(417, 255, 399, 205, 386, 245);
+    
+    ofDrawTriangle(429, 271, 447, 275, 421, 256);
+    
+    ofDrawTriangle(429, 271, 447, 275, 438, 278);
+    
+    ofDrawTriangle(438, 253, 461, 266, 425, 205);
+    
+    ofDrawTriangle(438, 253, 461, 266, 457, 271);
+    
+    ofDrawTriangle(438, 253, 452, 272, 457, 271);
+    
+    ofDrawTriangle(426, 204, 453, 239, 462, 230);
+    
+    ofDrawTriangle(426, 204, 453, 239, 461, 262);
+    
+    ofDrawTriangle(454, 240, 462, 233, 462, 260);
+    
+    ofDrawTriangle(464, 233, 470, 248, 463, 260);
+    
+    ofDrawTriangle(464, 233, 470, 248, 470, 238);
+    
+    ofDrawTriangle(353, 108, 360, 119, 354, 119);
+    
+    ofDrawTriangle(353, 108, 360, 119, 363, 83);
+    
+    ofDrawTriangle(373, 83, 360, 119, 363, 83);
+    
+    ofDrawTriangle(373, 83, 384, 67, 363, 83);
+    
+    ofDrawTriangle(373, 83, 384, 67, 383, 76);
+    
+    ofDrawTriangle(386, 70, 384, 67, 383, 76);
+    
+    ofDrawTriangle(386, 70, 384, 90, 383, 76);
+    
+    ofDrawTriangle(308, 193, 311, 229, 273, 295);
+    
+    ofDrawTriangle(308, 193, 311, 229, 318, 210);
+    
+    ofDrawTriangle(338, 228, 311, 229, 318, 210);
+    
+    ofSetColor(225, 42, 153);
+    
+    ofDrawTriangle(374, 83, 380, 80, 363, 116);
+    
+    ofDrawTriangle(378, 88, 373, 108, 364, 117);
+    
+    ofDrawTriangle(381, 82, 376, 105, 382, 95);
+    
+    ofSetColor(224, 229, 69);
+    
+    ofDrawTriangle(380, 113, 388, 109, 392, 131);
+    
+    ofDrawTriangle(390, 108, 405, 121, 393, 132);
+    
+    ofDrawTriangle(390, 108, 405, 121, 401, 102);
+    
+    ofDrawTriangle(403, 101, 418, 110, 408, 119);
+    
+    ofDrawTriangle(403, 101, 418, 110, 413, 95);
+    
+    ofDrawTriangle(416, 93, 430, 101, 421, 108);
+    
+    ofDrawTriangle(416, 93, 430, 101, 425, 87);
+    
+    ofDrawTriangle(428, 86, 438, 93, 432, 99);
+    
+    ofDrawTriangle(428, 86, 438, 93, 435, 82);
+    
+    ofDrawTriangle(438, 80, 448, 86, 441, 92);
+    
+    ofDrawTriangle(438, 80, 448, 86, 445, 77);
+    
+    ofDrawTriangle(448, 75, 455, 80, 450, 84);
+    
+    ofDrawTriangle(448, 75, 455, 80, 454, 72);
+    
+    ofDrawTriangle(456, 70, 462, 75, 456, 79);
+    
+    ofDrawTriangle(456, 70, 462, 75, 461, 67);
+    
+    ofDrawTriangle(463, 67, 467, 71, 464, 74);
+    
+    ofDrawTriangle(463, 67, 467, 71, 466, 64);
+    
+    ofDrawTriangle(468, 63, 471, 67, 468, 70);
+    
+    ofDrawTriangle(468, 63, 471, 67, 471, 61);
+    
+    ofDrawTriangle(473, 60, 475, 64, 473, 66);
+    
+    ofDrawTriangle(473, 60, 475, 64, 497, 46);
+    
+    ofSetColor(11, 197, 197);
+    
+    ofDrawEllipse(395, 157, 13, 13);
+    
+    ofSetColor(254, 228, 172);
+    
+    ofDrawTriangle(393, 166, 399, 202, 377, 160);
+    
+    ofDrawTriangle(393, 166, 399, 202, 405, 162);
+    
+    ofSetColor(174, 136, 211);
+    
+    ofDrawTriangle(405, 148, 464, 230, 423, 200);
+    
+    ofSetColor(255, 255, 255);
+    
+    ofDrawEllipse(395, 157, 5, 5);
+    
+    ofDrawEllipse(394, 159, 3, 3);
+    
+    ofSetColor(97, 38, 98);
+    
+    ofDrawTriangle(384, 149, 392, 150, 387, 152 );
+    
+     ofDrawTriangle(380, 153, 384, 155, 387, 152 );
+    
+    ofDrawTriangle(377, 158, 383, 158, 384, 155 );
+   
+    ofDrawTriangle(377, 158, 383, 158, 389, 162 );
+    
+     ofDrawTriangle(392, 150, 391, 146, 398, 150 );
+    
+    ofSetColor(205, 197, 177);
+    
+     ofDrawTriangle(396, 137, 397, 142, 352, 142 );
+    
+    ofDrawTriangle(396, 137, 397, 142, 406, 157 );
+    
+    
+    
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y ){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseEntered(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseExited(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo){ 
+
+}
