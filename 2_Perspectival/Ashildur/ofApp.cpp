07d9813aa95e9a0a30463d657257019a22bd69e0
Ace <31637250+ashildurf@users.noreply.github.com>
2018-02-09 00:45:19 -0500

    Add files via upload

---
+++ 2_Perspectival/Ashildur/ofApp.cpp
@@ -0,0 +1,163 @@
+#include "ofApp.h"
+
+//--------------------------------------------------------------
+void ofApp::setup(){
+    ofSetBackgroundAuto(false);
+}
+
+//--------------------------------------------------------------
+void ofApp::update(){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::draw(){
+    
+    //deepgreen
+    ofSetColor(49,114,86);
+    ofDrawTriangle(236, 313, 177, 309, 177, 329);
+    
+    //green
+    ofSetColor(65,152,115);
+    ofDrawTriangle(188, 700, 169, 189, 200, 189);
+    ofDrawTriangle(244, 289, 182, 285, 191, 251);
+     ofDrawTriangle(246, 282, 173, 277, 173, 306);
+    ofDrawTriangle(124, 310, 176, 300, 177, 324);
+    
+    
+    
+    
+    //purplebehindbehind
+    ofSetColor(162,138,210);
+    ofDrawTriangle(83, 276, 127, 139, 209, 191);
+    
+    //redbehindbehind
+    ofSetColor(220,0,5);
+    ofDrawTriangle(295, 265, 173, 250, 209, 191);
+    
+    //darkpinkbehind
+    
+    ofSetColor(178, 91, 110);
+    ofDrawTriangle(147, 265, 172, 174, 247, 176);
+    ofDrawTriangle(83, 276, 127, 170, 209, 191);
+    
+    //pink
+    
+    ofSetColor(226, 117, 137);
+    ofDrawTriangle(147, 265, 228, 157, 250, 175);
+     ofDrawTriangle(147, 265, 238, 235, 250, 175);
+    
+    //lightpinkbehind
+    ofSetColor(236,161,172);
+    ofDrawTriangle(147, 265, 172, 174, 105, 167);
+    
+    //purplebehind
+    ofSetColor(162,138,210);
+    //ofDrawTriangle(133, 81, 249, 178, 105, 167);
+    
+    //redbehind
+    ofSetColor(220,0,5);
+      ofDrawTriangle(91, 162, 228, 157, 136, 100);
+    ofDrawTriangle(256, 106, 142, 97, 200, 153);
+    
+    //darkpinkbehind
+    ofSetColor (178,91,110);
+    ofDrawTriangle(255, 106, 250, 179, 200, 153);
+    
+    //purple
+    ofSetColor (162,138,210);
+    ofDrawTriangle(250, 182, 166, 204, 175, 181);
+    
+    
+    //lightpink
+    
+    ofSetColor(236,161,172);
+    ofDrawTriangle(173, 125, 221, 113, 180, 150);
+    ofDrawTriangle(220, 150, 221, 113, 180, 150);
+    ofDrawTriangle(228, 175, 221, 113, 175, 181);
+    ofDrawTriangle(110, 153, 157, 106, 175, 181);
+    
+    
+    //pink
+    
+    ofSetColor(226, 117, 137);
+    ofDrawTriangle(159, 150, 175, 180, 180, 150);
+    ofDrawTriangle(218, 113, 157, 106, 175, 181);
+    
+    //darkpink
+    
+    ofSetColor(178, 91, 110);
+    ofDrawTriangle(202, 150, 175, 180, 180, 150);
+    ofDrawTriangle(202, 150, 205, 127, 180, 150);
+    
+  
+    
+    //red
+    ofSetColor(220, 0, 5);
+    ofDrawTriangle(173, 125, 159, 150, 180, 150);
+    ofDrawTriangle(173, 125, 205, 127, 180, 150);
+    ofDrawTriangle(107, 149, 103, 169, 175, 181);
+    ofDrawTriangle(165, 205, 107, 170, 175, 181);
+    ofDrawTriangle(250, 182, 242, 155, 175, 181);
+    ofDrawTriangle(260, 116, 230, 178, 222, 124);
+    ofDrawTriangle(260, 116, 230, 178, 259, 162);
+    
+   
+    
+}
+
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y ){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseEntered(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseExited(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo){ 
+
+}
