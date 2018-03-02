7ae2c8af2fd8642d2128c105fc3e29cb7e00be6f
Young Tae Yoon <yoony857@newschool.edu>
2018-03-02 10:52:59 -0500

    revised homework

--- 5_Zeno/YoungTae/src/ofApp.cpp
+++ 5_Zeno/YoungTae/src/ofApp.cpp
@@ -4,8 +4,9 @@
 float rect_catchUpSpeed = 0.01f;  // speed with which we pursue the mouse
 float rect1_catchUpSpeed = 0.02f;
 float rect2_catchUpSpeed = 0.03f;
-float rect3_catchUpSpeed = 0.04f;
-float rect4_catchUpSpeed = 0.05f;
+float rect2_maxspeed = 0.5f;
+float rect3_catchUpSpeed = 10;
+float rect4_maxspeed = 0.1f;
 ofPoint rect_pos;
 ofPoint rect1_pos;
 ofPoint rect2_pos;
@@ -16,6 +17,8 @@ ofPoint rect4_pos;
 void ofApp::setup(){
     // macs by default run on non vertical sync, which can make animation very, very fast
     // this fixes that:
+    
+    ofSetWindowShape(1000, 1000);
     ofSetVerticalSync(true);
 
     // set background:
@@ -33,14 +36,19 @@ void ofApp::update(){
     rect1_pos.x += rect1_catchUpSpeed * (mouseX - rect1_pos.x);
     rect1_pos.y += rect1_catchUpSpeed * (mouseY - rect1_pos.y);
     
-    rect2_pos.x += rect2_catchUpSpeed * (mouseX - rect2_pos.x);
-    rect2_pos.y += rect2_catchUpSpeed * (mouseY - rect2_pos.y);
+//    rect2_pos.x += rect2_catchUpSpeed * (mouseX - rect2_pos.x);
+//    rect2_pos.y += rect2_catchUpSpeed * (mouseY - rect2_pos.y);
     
+    if ((rect2_pos.x < mouseX)&&(rect2_pos.y < mouseY)) {
+        rect2_catchUpSpeed = rect2_maxspeed;
+    }
+
+
     rect3_pos.x += rect3_catchUpSpeed * (mouseX - rect3_pos.x);
     rect3_pos.y += rect3_catchUpSpeed * (mouseY - rect3_pos.y);
     
-    rect4_pos.x += rect4_catchUpSpeed * (mouseX - rect4_pos.x);
-    rect4_pos.y += rect4_catchUpSpeed * (mouseY - rect4_pos.y);
+    rect4_pos.x += rect4_maxspeed * (mouseX - rect4_pos.x);
+    rect4_pos.y += rect4_maxspeed * (mouseY - rect4_pos.y);
     
 
 
