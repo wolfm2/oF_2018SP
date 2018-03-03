8d44cca0dd83957691a5bb3aad4e66f4c72600cc
Rita <ritaaaazhang@163.com>
2018-03-02 21:11:20 -0500

    HMW

---
+++ 7_matrix/Ruixi/src/ofApp.cpp
@@ -0,0 +1,211 @@
+#include "ofApp.h"
+
+
+//--------------------------------------------------------------
+void ofApp::setup(){
+   
+    
+    ofBackground(255);
+    feather.load("images/feather.jpg");
+    font.load("font/font.otf",50);
+    
+}
+//--------------------------------------------------------------
+void ofApp::update(){
+    
+    
+    //here is change for leaf
+//  rX++;   //each frame changing
+//  if (ofGetFrameNum() % 3 == 0)  // 3 frames
+//    rY++;
+//  if (ofGetFrameNum() % 7 == 0)  //7 frames
+//    rZ++;
+//
+    
+    
+    
+    
+//    feather rotation
+//
+//    featherX ++;
+//
+//    if (ofGetFrameNum() % 5 == 0)
+//        featherY ++;
+//
+//    if (ofGetFrameNum() % 50 ==0)
+//        featherZ ++;
+//
+//
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::draw(){
+
+    
+    //here is rotate the leaf
+//ofPushMatrix();
+//  ofTranslate(ofGetWidth()/2, ofGetHeight()/2);
+//  ofScale(0.3, 0.3, 0.3);  // can be 3 dimensional
+//  ofRotateX(rX);
+//  ofRotateY(rY);
+//  ofRotateZ(rZ);
+//  leaf.draw(0 - leaf.getWidth()/2, 0 - leaf.getHeight()/2);  // draw at new 0,0
+
+    
+    ofSetColor(255, 10, 220);
+    ofFill();
+    font.drawString(" play with me ", 100, 150);
+//    ofSetColor(255, 255, 0);
+//    ofFill();
+//    font.drawString(" play with me ", 200, 200);
+//
+    
+    //rotate feather.
+    ofPushMatrix();
+    ofTranslate(ofGetWidth()/2, ofGetHeight()/2);
+    ofScale(1 + featherScaleW ,1 + featherScaleH ,1);
+    
+    
+    ofRotate(featherX);        //rotate x,y,z
+    ofRotate(featherY);
+    ofRotate(featherZ);
+    
+    ofNoFill();
+    feather.draw(0 - feather.getWidth()/2 + featherTranslateX , 0 - feather.getHeight()/2 + featherTranslateY);
+    
+    ofPopMatrix();
+    
+//    char fpsStr[255]; // an array of chars
+//    sprintf(fpsStr, "frame rate: %f", ofGetFrameRate());
+
+//    font.drawString(fpsStr, 100,100);
+
+ 
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key){
+    if ( key == 'x'){
+
+        featherX --;
+    }
+    
+    if( key == 'X'){
+
+        featherX ++;
+    }
+    
+    if( key == 'y'){
+
+        featherY --;
+    }
+    
+    if( key == 'Y'){
+
+        featherY ++;
+    }
+    
+    
+    if( key == 'z'){
+
+        featherZ --;
+    }
+    
+    if( key == 'Z'){
+
+        featherZ ++;
+    }
+    
+    
+    
+    if( key == 'w'){
+
+        featherScaleW -= 0.2;
+    }
+    
+    if( key == 'W'){
+
+        featherScaleW += 0.2;
+    }
+    
+    if( key == 'h'){
+
+        featherScaleH -= 0.2;
+    }
+    
+    if( key == 'H'){
+
+        featherScaleH += 0.2;
+    }
+    
+    if( key == 'a'){
+        featherTranslateX -= 1;
+    }
+    
+    if( key == 'd'){
+        featherTranslateX += 1;
+    }
+    
+    if( key == 'q'){
+        featherTranslateY -= 1;
+    }
+    
+    if( key == 's'){
+        featherTranslateY += 1;
+    }
+    
+
+
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseEntered(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseExited(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo){ 
+
+}
