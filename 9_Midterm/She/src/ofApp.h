dfae143a4ed16d3f6e85727e285dcf89b407e225
JingSHE <jings474@newschool.edu>
2018-03-24 10:00:01 -0400

    midterm work

---
+++ 9_Midterm/She/src/ofApp.h
@@ -0,0 +1,43 @@
+#pragma once
+
+#include "ofMain.h"
+#include "ofxJSON.h"
+
+class ofApp : public ofBaseApp{
+    
+
+	public:
+		void setup();
+		void update();
+		void draw();
+
+		void keyPressed(int key);
+		void keyReleased(int key);
+		void mouseMoved(int x, int y );
+		void mouseDragged(int x, int y, int button);
+		void mousePressed(int x, int y, int button);
+		void mouseReleased(int x, int y, int button);
+		void mouseEntered(int x, int y);
+		void mouseExited(int x, int y);
+		void windowResized(int w, int h);
+		void dragEvent(ofDragInfo dragInfo);
+		void gotMessage(ofMessage msg);
+    
+    ofxJSONElement json;
+    ofTrueTypeFont myFont;
+    ofImage image;
+    ofImage pinimage;
+    ofImage stone;
+    ofFbo fbo;
+    
+    string Location;
+    string DominantColor;
+    int MostPopular = 0;
+    string Address;
+    string Description;
+    int Num = 0;
+    int count = 0;
+    
+    
+    
+};
