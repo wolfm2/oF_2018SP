30dea17cbd32027dcc9815f9bc71ec999df0899f
Ace <31637250+ashildurf@users.noreply.github.com>
2018-03-26 16:56:22 -0400

    adding zeno

--- 5_Zeno/Ashildur/src/ofApp.cpp
+++ 5_Zeno/Ashildur/src/ofApp.cpp
@@ -2,7 +2,9 @@
 #include "ofMain.h"
 
 float rect_catchUpSpeed = 0.01f;  // speed with which we pursue the mouse
-ofPoint rect_pos;
+ofPoint rect_pos; //ofPoint is a vector
+ofPoint rect2_pos;
+float rect2_catchUpSpeed = 0.015f;
 
 //--------------------------------------------------------------
 void ofApp::setup(){
@@ -28,17 +30,30 @@ void ofApp::update(){
     
     rect_pos.x = rect_catchUpSpeed * mouseX + (1-rect_catchUpSpeed) * rect_pos.x;
     rect_pos.y = rect_catchUpSpeed * mouseY + (1-rect_catchUpSpeed) * rect_pos.y;
-
+    
+    rect2_pos.x += rect2_catchUpSpeed * (mouseX - rect2_pos.x);
+    rect2_pos.y += rect2_catchUpSpeed * (mouseY - rect2_pos.y);
     
     
 }
 
 //--------------------------------------------------------------
 void ofApp::draw(){
+    
+    ofBackground(100, 100, 160);
+    
+    
     ofFill();
     ofSetRectMode(OF_RECTMODE_CENTER); // center around the position
     ofSetColor(198,246,55);
     ofRect(rect_pos.x,rect_pos.y, 20,20);
+   
+    
+    
+    ofSetColor(234, 124, 144);
+    ofDrawCircle(mouseX, mouseY, 20); //This makes the circle follow the mouse
     
 
 }
+
+
