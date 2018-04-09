cf87904205c4e969a2dc7e06cb6534294f00c9d9
zhanr526 <30604400+zhanr526@users.noreply.github.com>
2018-04-08 22:25:35 -0400

    src

---
+++ 10_objects/ZhangRuixi/ofApp.h
@@ -0,0 +1,22 @@
+#ifndef _TEST_APP
+#define _TEST_APP
+
+#include "ofMain.h"
+#include "square.hpp"
+#define  squareNum 5
+
+class ofApp : public ofSimpleApp{
+
+	public:
+
+		void setup();
+		void update();
+		void draw();
+    
+    
+    Square mySquare[squareNum];
+    
+    
+};
+
+#endif
